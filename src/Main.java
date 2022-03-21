cea5bb3a8562fa13cc923cb16766d8f209992d83
sol <sol.github@gmail.com>
2022-03-21 23:20:27 +0100

    Cleanup Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,18 @@
-import com.sun.javafx.perf.PerformanceTracker;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import simulation.Simulation;
 
-
 public class Main extends Application {
-    private static PerformanceTracker tracker;
-
-    private float getFPS () {
-        float fps = tracker.getAverageFPS();
-        tracker.resetAverageFPS();
-        return fps;
-    }
 
     @Override
     public void start(Stage stage) {
         Simulation simulation = new Simulation();
         simulation.populateSimulation(  10);
+
         Pane context = simulation.getGrid().getContext();
-
-        /*VBox root = new VBox(20);
-        Label label1 = new Label();
-        Label label2 = new Label();
-        root.getChildren().addAll(label1, label2);
-        context.getChildren().addAll(root);*/
-
         Scene scene = new Scene(context);
 
         Thread test = new Thread(() -> {
@@ -57,8 +40,3 @@ public class Main extends Application {
         launch(args);
     }
 }
-                /*long t0 = System.nanoTime();
-                AtomicLong frame = new AtomicLong();
-                simulation.update();
-                long t1 = System.nanoTime();
-                Platform.runLater(() -> label1.setText("F" + frame.incrementAndGet() + " >>> " + (t1 - t0) / 1e6 + " ms."));*/
